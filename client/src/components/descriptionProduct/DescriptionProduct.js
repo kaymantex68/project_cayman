58ccb2bacfbf98366cd455dbd0fc0f5ce68293e1
Andrey Sergeevich <andrey.s.h.68@yandex.ru>
2021-04-01 17:39:01 +0300

    description card

--- client/src/components/descriptionProduct/DescriptionProduct.js
+++ client/src/components/descriptionProduct/DescriptionProduct.js
@@ -3,28 +3,88 @@ import NavMenu from '../nav/NavMenu'
 import Footer from '../footer/Footer'
 import { getProduct } from '../../functions/product'
 import classes from './DescriptioProduct.module.css'
+// Import Swiper styles
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Thumbs } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Thumbs]);
+
 
 const DescriptionProduct = ({ match }) => {
     const [product, setProduct] = useState([])
+    const [swiper, setSwiper] = useState([])
     console.log(match.params)
     useEffect(() => {
         getProduct(match.params.name).then(res => {
             setProduct(res.data)
+            const slides = [];
+            let pathImage;
+            if (res.data.images.length>0) {
+                for (let i = 0; i < res.data.images.length; i += 1) {
+                    pathImage = `${process.env.REACT_APP_IMAGES_PRODUCTS}/${res.data.images[i]}`;
+                    slides.push(
+                        <SwiperSlide key={`descriptionProduct-${i}`} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                            <img
+                                alt={`${res.data.name}`}
+                                src={pathImage}
+                                style={{ maxWidth: "60%" }}
+                            />
+                        </SwiperSlide>
+                    );
+                }
+            } else {
+                pathImage = `${process.env.REACT_APP_IMAGES_PRODUCTS}/default.png`;
+                slides.push(
+                    <SwiperSlide  style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <img
+                            alt={`${res.data.name}`}
+                            src={pathImage}
+                            style={{ maxWidth: "60%" }}
+                        />
+                    </SwiperSlide>
+                );
+            }
+            setSwiper([...slides])
         })
     }, [])
-    console.log('product', product)
+
+
+
+    console.log('swiper', swiper)
+
+
     return (
         <>
             <div>
                 <NavMenu />
             </div>
             <div className={classes.Container} >
-                <div className={classes.picturePhoto}></div>
+                <div className={classes.picturePhoto}>
+                    {/*start slider */}
+                    <div className={classes.swiperContainer}>
+                        <Swiper
+                            spaceBetween={0}
+                            slidesPerView={1}
+                            navigation
+                            pagination={{ clickable: true }}
+                            scrollbar={{ draggable: true }}
+                            loop={true}
+                            className={classes.Swiper_cont}
+                        >
+                            {swiper}
+                        </Swiper>
+                    </div>
+                    {/* end slider */}
+                </div>
 
                 <div className={classes.descriptionContainer}>
                     <div className={classes.name_coast_container}>
                         <div style={{ fontSize: "1.2rem", fontWeight: "bold" }}>{product.name}</div>
-                        <div style={{ fontSize: "1rem", fontWeight:"bold" }}>Цена: {product.coast} руб.</div>
+                        <div style={{ fontSize: "1rem", fontWeight: "bold" }}>Цена: {product.oldCoast? <><span style={{color:"red", textDecoration:"line-through"}}>{product.oldCoast}</span>  <span style={{ fontSize:"1.2rem"}}>{product.coast}</span></> :<span>{product.coast}</span>} руб.</div>
                     </div>
                     <hr />
                     <div>
